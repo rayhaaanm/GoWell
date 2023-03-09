79753fb73ab7cfdb07c1df78f90e1032000597e7
rayhaaanm <rayhan.azazaka.firdaus-2021@fst.unair.ac.id>
2023-03-09 09:42:12 +0700

    analytics changes

--- Project_1/js/calculator.js
+++ Project_1/js/calculator.js
@@ -6,11 +6,11 @@ function hitungBeratBadanIdeal()
      
                 var Hitung = ((TinggiBadan - 100)-((TinggiBadan - 100)*(10/100)));
                 if (Hitung == BeratBadan){
-                    hs = " ideal"
+                    hs = "Your result suggests you are a healthy weight \nWe suggest you maintain your weight. We've got lots of workouts and activity ideas to help you stay active."
                 } else  if (Hitung < BeratBadan){
-                    hs="kelebihan "} 
+                    hs="Your result suggests you are overweight \nYour health would really benefit from gradually losing just 5% of your current weight. The best way to lose weight is through a combination of diet and exercise.\nWe've got lots of resources to help you lose weight safely. If you're concerned about your weight speak to your GP."} 
                     else {
-                        hs = "kekurangan"
+                        hs = "Your result suggests you are underweight\nIf you are receiving treatment for an eating disorder then this tool is NOT to be used \nThere may be an underlying medical cause for your weight, or your diet may not be providing you with enough calories. We suggest you discuss this with your GP."
                             }  
                 document.fr.Hasil.value = hs;  
 };
