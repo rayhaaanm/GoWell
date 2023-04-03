cce9277f69eaddba2517d6d4b4772fca757d70cf
rayhaaanm <rayhan.azazaka.firdaus-2021@fst.unair.ac.id>
2023-04-03 11:07:48 +0700

    add upload profile picture

--- Project_1/js/setting.js
+++ Project_1/js/setting.js
@@ -6,3 +6,14 @@ function CloseForm(){
     document.getElementById("pop-up").style.display = "none";
     document.getElementById("bg").style.display = "none";
 }
+function OpenUpload(){
+    var input = document.getElementById('upload');
+    input.click();
+}
+
+function EditProfile(event){
+    var image = document.getElementById('profile-picture-edit');
+    var image2 = document.getElementById('profile-picture');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    image2.src = URL.createObjectURL(event.target.files[0]);
+}
